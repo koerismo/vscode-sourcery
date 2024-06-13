15bb848ca807c029f6a1bef10d37b77320a0bf52
Koerismo <56563658+koerismo@users.noreply.github.com>
2024-06-13 19:09:06 -0400

    feat: buh1

--- public/src/vmt-editor/index.ts
+++ public/src/vmt-editor/index.ts
@@ -1,4 +1,26 @@
 import { Main } from '../shared/Main';
 
+declare function acquireVsCodeApi(): { postMessage(message: any): void };
+
+const vscode = acquireVsCodeApi();
+
+export function modFetch(resolvedPath: string): Promise<Uint8Array | null> {
+	return new Promise((resolve) => {
+		const onMessage = (event: MessageEvent): void => {
+			const message = event.data;
+			if (message.path !== resolvedPath) return;
+			removeEventListener('message', onMessage);
+			console.log('RESOLVED!', message);
+
+			const buf = message.data;
+			if (!buf) return resolve(null);
+			resolve(buf);
+		};
+
+		addEventListener('message', onMessage);
+		vscode.postMessage({ path: resolvedPath, fromSelf: true });
+	});
+}
+
 // God help us all
 const main = new Main();
