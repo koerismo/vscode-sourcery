6b66b375e0274534c07d20c23d83b3425f135107
Koerismo <56563658+koerismo@users.noreply.github.com>
2024-06-25 17:24:38 -0400

    feat: Remove noclip from source files

--- public/src/shared/noclip/rustlib.ts
+++
@@ -1,8 +0,0 @@
-
-export let rust: typeof import('../rust/pkg/index.js') = null!;
-
-export async function loadRustLib() {
-    if (rust === null) {
-        rust = await import('../rust/pkg/index.js');
-    }
-}
